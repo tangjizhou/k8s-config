521a83309afc88616e54fea54ebff80384aa933e
tangjizhou <tangjizhouchn@foxmail.com>
2019-09-27 20:46:22 +0800

    init

---
+++ src/test/java/net/mshome/twisted/k8syaml/K8sYamlApplicationTests.java
@@ -0,0 +1,16 @@
+package net.mshome.twisted.k8syaml;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class K8sYamlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
