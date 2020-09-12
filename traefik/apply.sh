09f22b2586641a0d9c7aa5f14fe4370b056205cf
tangjizhou <tangjizhouchn@foxmail.com>
2020-09-12 23:22:32 +0800

    update

---
+++ traefik/apply.sh
@@ -0,0 +1,4 @@
+kubectl apply -f https://www.qikqiak.com/k8strain/network/manifests/traefik/crd.yaml
+kubectl apply -f https://www.qikqiak.com/k8strain/network/manifests/traefik/rbac.yaml
+kubectl apply -f https://www.qikqiak.com/k8strain/network/manifests/traefik/deployment.yaml
+kubectl apply -f https://www.qikqiak.com/k8strain/network/manifests/traefik/dashboard.yaml
